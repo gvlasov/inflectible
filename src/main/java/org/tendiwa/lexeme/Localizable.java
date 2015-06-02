c0714d9c5453460c75c579b165fd5a6e4df0dacb
Suseika <wlasowegor@gmail.com>
2015-06-02 18:25:11 +0500

    Doc for Localizable

--- src/main/java/org/tendiwa/lexeme/Localizable.java
+++ src/main/java/org/tendiwa/lexeme/Localizable.java
@@ -1,5 +1,10 @@
 package org.tendiwa.lexeme;
 
+/**
+ * Allows naming things so sames are translateable to different languages.
+ *
+ * @see org.tendiwa.lexeme
+ */
 public interface Localizable {
-public String getLocalizationId();
+	public String getLocalizationId();
 }
