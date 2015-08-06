f864a38c691f11cdc56bbedadde6cd968616a2e4
Suseika <wlasowegor@gmail.com>
2015-08-06 16:17:37 +0500

    #43 Fix capitalization of an asserted string

--- src/test/java/org/tendiwa/lexeme/ParsedLexemeMarkupTest.java
+++ src/test/java/org/tendiwa/lexeme/ParsedLexemeMarkupTest.java
@@ -42,7 +42,7 @@ public final class ParsedLexemeMarkupTest {
         );
         MatcherAssert.assertThat(
             markup.persistentGrammemes(),
-            CoreMatchers.equalTo(ImmutableList.of("grammeme"))
+            CoreMatchers.equalTo(ImmutableList.of("Grammeme"))
         );
         MatcherAssert.assertThat(
             markup.wordForms().isEmpty(),
