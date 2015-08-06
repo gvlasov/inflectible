1deb0b80fe65e598b533e0fd8594259a0e918a8f
Suseika <wlasowegor@gmail.com>
2015-08-06 15:44:43 +0500

    #43 Create test for ParsedWordFormMarkup and the class it tests

--- src/main/java/org/tendiwa/lexeme/ParsedWordFormMarkup.java
+++ src/main/java/org/tendiwa/lexeme/ParsedWordFormMarkup.java
@@ -1,9 +1,29 @@
 package org.tendiwa.lexeme;
 
+import org.tendiwa.lexeme.antlr.WordBundleParser;
+import org.tendiwa.rocollections.ReadOnlyList;
+
 /**
+ * WordFormMarkup parsed from an ANTLR parse tree.
  * @author Georgy Vlasov (suseika@tendiwa.org)
  * @version $Id$
  * @since 0.1
  */
-public final class ParsedWordFormMarkup {
+final class ParsedWordFormMarkup implements WordFormMarkup {
+    private final WordBundleParser.WordContext ctx;
+
+    ParsedWordFormMarkup(
+        WordBundleParser.WordContext ctx
+    ) {
+        this.ctx = ctx;
+    }
+    @Override
+    public String spelling() {
+        return null;
+    }
+
+    @Override
+    public ReadOnlyList<String> grammemes() {
+        return null;
+    }
 }
---
+++ src/test/java/org/tendiwa/lexeme/ParsedWordFormMarkupTest.java
@@ -0,0 +1,42 @@
+package org.tendiwa.lexeme;
+
+import org.antlr.v4.runtime.ANTLRInputStream;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.apache.commons.io.IOUtils;
+import org.hamcrest.CoreMatchers;
+import org.hamcrest.MatcherAssert;
+import org.junit.Test;
+import org.tendiwa.lexeme.antlr.WordBundleLexer;
+import org.tendiwa.lexeme.antlr.WordBundleParser;
+
+/**
+ * @since 0.1
+ */
+public final class ParsedWordFormMarkupTest {
+    @Test
+    public void seesAllComponents() throws Exception {
+        final WordFormMarkup markup = new ParsedWordFormMarkup(
+            new WordBundleParser(
+                new CommonTokenStream(
+                    new WordBundleLexer(
+                        new ANTLRInputStream(
+                            IOUtils.toInputStream(
+                                "  dudes [Plur]"
+                            )
+                        )
+                    )
+                )
+            )
+                .word()
+        );
+        MatcherAssert.assertThat(
+            markup.spelling(),
+            CoreMatchers.is("dudes")
+        );
+        MatcherAssert.assertThat(
+            markup.grammemes().contains("Plur"),
+            CoreMatchers.is(true)
+        );
+    }
+
+}
