bc417c08c05d5c97b0d0d54c1c0c142506687c39
Suseika <suseika@tendiwa.org>
2015-08-12 20:05:56 +0500

    #66 Rewrite parsed two part placeholder delegate to a BasicPlaceholder

--- src/main/java/org/tendiwa/lexeme/ParsedTwoPartVariableConceptPlaceholder.java
+++ src/main/java/org/tendiwa/lexeme/ParsedTwoPartVariableConceptPlaceholder.java
@@ -1,10 +1,11 @@
 package org.tendiwa.lexeme;
 
 import com.google.common.collect.ImmutableSet;
-import java.util.Optional;
 import org.antlr.v4.runtime.tree.ParseTree;
 import org.tendiwa.lexeme.antlr.TextBundleParser;
 import org.tenidwa.collections.utils.Collectors;
+
+import java.util.Optional;
 
 /**
  * @author Georgy Vlasov (suseika@tendiwa.org)
@@ -12,7 +13,7 @@ import org.tenidwa.collections.utils.Collectors;
  * @since 0.1
  */
 final class ParsedTwoPartVariableConceptPlaceholder
-    extends AbstractVariableConceptPlaceholder {
+        implements FillablePlaceholder {
     private final Grammar grammar;
 
     private final TextBundleParser.PlaceholderContext ctx;
@@ -26,29 +27,20 @@ final class ParsedTwoPartVariableConceptPlaceholder
         this.ctx = ctx;
     }
 
-    @Override
-    public ImmutableSet<Grammeme> grammaticalMeaning() {
+    private FillablePlaceholder delegate() {
+        return new BasicPlaceholder(
+            this.name(),
+            this.grammemes(),
+            this.agreementArgumentName()
+        );
+    }
+
+    private ImmutableSet<Grammeme> grammemes() {
         return this.ctx.GRAMMEME()
             .stream()
             .map(ParseTree::getText)
             .map(this.grammar::grammemeByName)
             .collect(Collectors.toImmutableSet());
-    }
-
-    @Override
-    public String fillUp(ActualArguments arguments) {
-        final Placeholder placeholder;
-        if (this.agreementArgumentName().isPresent()) {
-            placeholder = new AgreeingPlaceholder(
-                arguments.argumentValue(
-                    this.agreementArgumentName().get()
-                )
-            );
-        } else {
-            placeholder = this;
-        }
-        return arguments.argumentValue(this.argumentName())
-            .formForPlaceholder(placeholder);
     }
 
     private Optional<String> agreementArgumentName() {
@@ -59,37 +51,17 @@ final class ParsedTwoPartVariableConceptPlaceholder
         }
     }
 
-    @Override
-    protected String identifier() {
+    private String name() {
         return this.ctx.CAPITALIZABLE_ID().getText();
     }
 
-    /**
-     * @author Georgy Vlasov (suseika@tendiwa.org)
-     * @version $Id$
-     * @since 0.1
-     */
-    private final class AgreeingPlaceholder implements Placeholder {
-        private final Lexeme lexeme;
+    @Override
+    public String fillUp(ActualArguments arguments) {
+        return this.delegate().fillUp(arguments);
+    }
 
-        public AgreeingPlaceholder(Lexeme lexeme) {
-            this.lexeme = lexeme;
-        }
-
-        @Override
-        public ImmutableSet<Grammeme> grammaticalMeaning() {
-            return ImmutableSet.<Grammeme>builder()
-                .addAll(
-                    ParsedTwoPartVariableConceptPlaceholder.this
-                        .grammaticalMeaning()
-                )
-                .addAll(this.lexeme.persistentGrammemes())
-                .build();
-        }
-
-        @Override
-        public boolean capitalizes() {
-            return ParsedTwoPartVariableConceptPlaceholder.this.capitalizes();
-        }
+    @Override
+    public ImmutableSet<Grammeme> grammaticalMeaning() {
+        return this.delegate().grammaticalMeaning();
     }
 }
