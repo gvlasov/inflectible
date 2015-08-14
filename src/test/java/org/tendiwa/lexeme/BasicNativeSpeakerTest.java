ba68a2841f40bdf07a89117ee7196610b0e6b3d0
Suseika <suseika@tendiwa.org>
2015-08-14 21:27:29 +0500

    #9 Add test for BasicNativeSpeaker

---
+++ src/test/java/org/tendiwa/lexeme/BasicNativeSpeakerTest.java
@@ -0,0 +1,31 @@
+package org.tendiwa.lexeme;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import org.hamcrest.CoreMatchers;
+import org.hamcrest.MatcherAssert;
+import org.junit.Test;
+
+public final class BasicNativeSpeakerTest {
+    @Test
+    public void fillsOutText() throws Exception {
+        MatcherAssert.assertThat(
+            new BasicNativeSpeaker(
+                ImmutableMap.of("bear", new SingleFormLexeme("bear")),
+                ImmutableMap.of(
+                    "attack.bite",
+                    new TextTemplateBuilder(ImmutableList.of("attacker"))
+                        .addPlaceholder(
+                            new BasicPlaceholder(
+                                new ArgumentsLexemeSource("attacker")
+                            )
+                                .withCapitalization(Capitalization.CAPITALZES)
+                        )
+                        .addText(" furiously bites YOU!")
+                        .build()
+                )
+            ).text("attack.bite", () -> "bear"),
+            CoreMatchers.equalTo("Bear furiously bites YOU!")
+        );
+    }
+}
