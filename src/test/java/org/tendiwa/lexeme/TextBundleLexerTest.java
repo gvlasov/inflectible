f4ef9e43ccca5b176ef5f8b9e78a3a60a18778f6
Suseika <suseika@tendiwa.org>
2015-08-14 23:47:38 +0500

    #83 Replace commons-lang code with Guava code

--- src/test/java/org/tendiwa/lexeme/TextBundleLexerTest.java
+++ src/test/java/org/tendiwa/lexeme/TextBundleLexerTest.java
@@ -1,8 +1,8 @@
 package org.tendiwa.lexeme;
 
+import com.google.common.base.Joiner;
 import org.antlr.v4.runtime.ANTLRInputStream;
 import org.apache.commons.io.IOUtils;
-import org.apache.commons.lang3.StringUtils;
 import org.hamcrest.CoreMatchers;
 import org.hamcrest.MatcherAssert;
 import org.junit.Test;
@@ -11,7 +11,7 @@ import org.tendiwa.lexeme.antlr.TextBundleLexer;
 public class TextBundleLexerTest {
     @Test
     public void splitsIntoTokens() throws Exception {
-        final String text = StringUtils.join(
+        final String text = Joiner.on('\n').join(
             "log.get_damage(attacker, action, aim) {",
             "    // Медведь кусает человека, человек получает пизды",
             "    [Attacker] [action][III] [aim][Р], [aim] получает пизды",
