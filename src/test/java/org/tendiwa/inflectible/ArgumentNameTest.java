7a39b32f909eb862ce6841d18fd4da99019a2390
Suseika <suseika@tendiwa.org>
2015-08-18 23:49:43 +0500

    #110 Implement and test ArgumentName

---
+++ src/test/java/org/tendiwa/inflectible/ArgumentNameTest.java
@@ -0,0 +1,58 @@
+/**
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2015 Georgy Vlasov
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package org.tendiwa.inflectible;
+
+import org.hamcrest.CoreMatchers;
+import org.hamcrest.MatcherAssert;
+import org.junit.Test;
+
+/**
+ * Unit tests for {@link ArgumentName}.
+ * @author Georgy Vlasov (suseika@tendiwa.org)
+ * @version $Id$
+ * @since 0.1
+ */
+public final class ArgumentNameTest {
+    /**
+     * {@link ArgumentName} can be created from a lowercase string.
+     * @throws Exception If fails
+     */
+    @Test
+    public void allowsLowercase() throws Exception {
+        final String name = "dude";
+        MatcherAssert.assertThat(
+            new ArgumentName(name).string(),
+            CoreMatchers.is(name)
+        );
+    }
+
+    /**
+     * {@link ArgumentName} can not be created from an uppercase string.
+     * @throws Exception If argument name didn't pass validation
+     */
+    @Test(expected = Exception.class)
+    public void disallowsNonLowercase() throws Exception {
+        new ArgumentName("DUDE").string();
+    }
+}
