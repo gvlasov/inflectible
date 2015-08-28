ad7f8a4b73f671205f819591a5f78da06d99d01d
Suseika <suseika@tendiwa.org>
2015-08-28 21:02:17 +0500

    #26 Create tests for GrammaticalCategory implementations

---
+++ src/test/java/org/tendiwa/inflectible/implementations/RussianGrammaticalCategoryTest.java
@@ -0,0 +1,68 @@
+/**
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2015 Georgy Vlasov
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package org.tendiwa.inflectible.implementations;
+
+import org.hamcrest.CoreMatchers;
+import org.hamcrest.MatcherAssert;
+import org.junit.Test;
+
+/**
+ * Unit tests for {@link RussianGrammaticalCategory}.
+ * @author Georgy Vlasov (suseika@tendiwa.org)
+ * @version $Id$
+ * @since 0.2.0
+ */
+public final class RussianGrammaticalCategoryTest {
+    /**
+     * {@link RussianGrammaticalCategory} can tell if it contains a particular
+     * grammeme or not.
+     * @throws Exception If fails
+     */
+    @Test
+    public void containsGrammemes() throws Exception {
+        MatcherAssert.assertThat(
+            RussianGrammaticalCategory.Падеж
+                .containsGrammeme(RussianGrammeme.И),
+            CoreMatchers.is(true)
+        );
+        MatcherAssert.assertThat(
+            RussianGrammaticalCategory.Падеж
+                .containsGrammeme(RussianGrammeme.Дееприч),
+            CoreMatchers.is(false)
+        );
+    }
+
+    /**
+     * {@link RussianGrammaticalCategory} can tell its default grammeme.
+     * @throws Exception If fails
+     */
+    @Test
+    public void hasDefaultGrammeme() throws Exception {
+        MatcherAssert.assertThat(
+            RussianGrammaticalCategory.Число
+                .defaultGrammeme(),
+            CoreMatchers.is(RussianGrammeme.Ед)
+        );
+    }
+}
